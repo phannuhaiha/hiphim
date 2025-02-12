63e43132a18e4e074281a92e1bb320ac1f5ab1da
phannuhaiha <phanhaiha.nu03@gmail.com>
2025-02-12 07:49:06 +0000

    ha day len ne

--- src/components/users/Header.vue
+++ src/components/users/Header.vue
@@ -2,32 +2,32 @@
   <div class="relative">
     <!-- Header -->
     <div :class="isDarkMode ? 'dark' : ''"
-      class="flex fixed z-50 w-screen justify-between items-center transition-all duration-300  p-2 bg-white dark:bg-gray-500 text-black dark:text-white shadow-bottom space-x-2">
+      class="flex fixed z-50 w-screen justify-between items-center transition-all duration-300 p-2 bg-white dark:bg-gray-500 text-black dark:text-white shadow-bottom space-x-2">
+      
       <!-- Góc trái: Menu Icon và Logo -->
       <div class="flex flex-row items-center space-x-1 md:space-x-4">
         <!-- Menu Icon (Hamburger) -->
-         <div class="sm:block hidden">
-          <div @click="$emit('toggle-menu')" class="flex flex-col w-1/5 h-full  ml-1 md:ml-4 gap-1 cursor-pointer ">
-          <i class="bx bx-menu text-xl sm:text-4xl"></i>
+        <div class="sm:block hidden">
+          <div @click="$emit('toggle-menu')" class="flex flex-col w-1/5 h-full ml-1 md:ml-4 gap-1 cursor-pointer">
+            <i class="bx bx-menu text-xl sm:text-4xl"></i>
+          </div>
         </div>
-         </div>
-        
 
-        <!-- Logo -->
+        <!-- Logo hoặc Avatar -->
         <router-link to="/product">
           <div class="sizelogo bg-auto rounded-full py-1 sm:py-2 text-white font-bold">
-            <img src="../icons/NewLogo.png" class="flex w-full h-full ">
+            <!-- Hiển thị logo khi chưa đăng nhập, avatar khi đã đăng nhập -->
+            <img v-if="!user" src="../icons/NewLogo.png" class="flex w-full h-full" />
+            <img v-else :src="user.avatar" class="flex w-full h-full rounded-full" />
           </div>
         </router-link>
       </div>
 
-      <!-- Ở giữa: Thanh tìm kiếm -->
+      <!-- Thanh tìm kiếm -->
       <div class="flex flex-grow mx-auto items-center">
-        <!-- Input tìm kiếm -->
         <div class="flex w-full px-3 sm:px-10 lg:px-32 items-center justify-center space-x-1">
           <input type="text" placeholder="Tìm kiếm"
             class="w-full py-0 md:py-2 px-2 md:px-4 border border-gray-500 rounded-full dark:text-black focus:outline-none" />
-          <!-- Nút tìm kiếm -->
           <button
             class="flex p-2 w-4 h-4 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-12 lg:h-12 items-center justify-center rounded-full hover:bg-gray-300 dark:hover:bg-gray-800 focus:outline-none">
             <i class="bx bx-search text-xl sm:text-3xl"></i>
@@ -35,18 +35,17 @@
         </div>
       </div>
 
-      <!-- Góc phải: Các biểu tượng khác -->
+      <!-- Các biểu tượng bên phải -->
       <div class="clearfix gap-3 pr-3 hidden sm:block">
         <!-- Icon Video -->
         <div class="float-left relative sm:mr-3" ref="videoMenu">
           <button @click.stop="toggleVideoMenu" class="w-10 h-10 rounded-full hover:bg-gray-300 dark:hover:bg-gray-800">
             <i class="bx bx-video text-lg sm:text-3xl"></i>
           </button>
-          <!-- Dropdown Menu -->
-          <div v-if="isVideoMenuOpen" @click.stop class="absolute right-0 mt-2 w-48 bg-white shadow-md rounded-md z-50 font-roboto">
+          <div v-if="isVideoMenuOpen" @click.stop class="absolute right-0 mt-2 w-48 bg-white shadow-md rounded-md z-50">
             <ul class="py-1">
               <li>
-                <button @click="handleOnlineVideo" class="block w-full text-left dark:text-black  px-4 py-2 hover:bg-gray-100">
+                <button @click="handleOnlineVideo" class="block w-full text-left dark:text-black px-4 py-2 hover:bg-gray-100">
                   Video trực tuyến
                 </button>
               </li>
@@ -60,22 +59,21 @@
         </div>
 
         <!-- Icon Bell -->
-         <div class="float-left sm:mr-3">
+        <div class="float-left sm:mr-3">
           <button class="w-10 h-10 rounded-full hover:bg-gray-300 dark:hover:bg-gray-800">
             <i class="bx bx-bell text-lg sm:text-3xl"></i>
           </button>
-         </div>
-        
+        </div>
 
         <!-- Avatar -->
-        <div class="float-left relative sm:mr-3" >
+        <div class="float-left relative sm:mr-3">
           <button @click="handleAvatarClick" class="w-10 h-10 rounded-full hover:bg-gray-300 dark:hover:bg-gray-800">
             <img v-if="user && user.avatar" :src="user.avatar" alt="Avatar" class="w-full h-full rounded-full" />
-            <i v-else class="bx bx-user-circle  text-lg sm:text-3xl"></i>
+            <i v-else class="bx bx-user-circle text-lg sm:text-3xl"></i>
           </button>
 
           <!-- Dropdown menu -->
-          <div v-if="user && isAvatarMenuOpen"  class="absolute right-0 mt-2 w-48 bg-white shadow-md rounded-md z-50">
+          <div v-if="user && isAvatarMenuOpen" ref="avatarMenu" class="absolute right-0 mt-2 w-48 bg-white shadow-md rounded-md z-50">
             <ul class="py-2">
               <li class="px-4 py-2 text-sm text-gray-700 hover:bg-gray-300 dark:hover:bg-gray-800">
                 <div>{{ user.name }}</div>
@@ -132,21 +130,27 @@ export default {
     toggleVideoMenu() {
       this.isVideoMenuOpen = !this.isVideoMenuOpen;
     },
-    handleAvatarClick() {
+    handleAvatarClick(event) {
+      // Nếu chưa đăng nhập, chuyển đến trang login
       if (!this.user) {
-        // Chưa đăng nhập, chuyển đến trang login
         this.$router.push({ name: 'login' });
       } else {
-        // Đã đăng nhập, mở menu
+        // Đã đăng nhập, mở hoặc đóng menu avatar
         this.isAvatarMenuOpen = !this.isAvatarMenuOpen;
       }
+      // Dừng sự kiện để không đóng dropdown khi nhấn vào avatar
+      event.stopPropagation();
     },
     closeAllDropdown(event) {
-     
-    if (this.$refs.videoMenu && !this.$refs.videoMenu.contains(event.target)) {
-      this.isVideoMenuOpen = false;
-    }
-  },
+      // Đóng menu video nếu nhấn ngoài menu video
+      if (this.$refs.videoMenu && !this.$refs.videoMenu.contains(event.target)) {
+        this.isVideoMenuOpen = false;
+      }
+      // Đóng menu avatar nếu nhấn ngoài menu avatar
+      if (this.$refs.avatarMenu && !this.$refs.avatarMenu.contains(event.target)) {
+        this.isAvatarMenuOpen = false;
+      }
+    },
     handleAccount() {
       window.open('https://myaccount.google.com/', '_blank');
     },
@@ -156,39 +160,33 @@ export default {
     logout() {
       this.user = null;
       location.reload();
-      csrf.removeCookie()
+      csrf.removeCookie();
     },
     handleOnlineVideo() {
       alert('Đang hiển thị Video trực tuyến!');
       this.isVideoMenuOpen = false;
     },
-    // async getUserInfo() {
-    //   try {
-    //     const token = csrf.getCookie()
-    //     const response = await axios.get('user/info', {
-    //       headers: {
-    //         "Authorization": `Bearer ${token}`
-    //       }
-    //     })
-    //     this.user = response.user
-    //     console.log(response)
-    //   } catch (error) {
-    //     console.log(error)
-    //   }
-    // },
-
+    async getUserInfo() {
+      try {
+        const token = csrf.getCookie();
+        const response = await axios.get('user/info', {
+          headers: {
+            "Authorization": `Bearer ${token}`,
+          },
+        });
+        this.user = response.data.user;
+      } catch (error) {
+        console.error(error);
+      }
+    },
   },
   async mounted() {
-
-    this.user = await checkAuthStatus()
-
+    this.user = await checkAuthStatus();
+    document.addEventListener("click", this.closeAllDropdown);
   },
-  mounted() {
-  document.addEventListener("click", this.closeAllDropdown);
-},
-beforeUnmount() {
-  document.removeEventListener("click", this.closeAllDropdown);
-}
+  beforeUnmount() {
+    document.removeEventListener("click", this.closeAllDropdown);
+  },
 };
 </script>
 
@@ -209,22 +207,18 @@ beforeUnmount() {
 
 /* sm */
 @media (min-width: 640px) {
-
   .menu {
     width: 25px;
     height: 2px;
     background-color: black;
   }
-
   .sizelogo {
     width: 100px;
     height: 50px;
   }
-
   .searchsize {
     max-width: 400px;
   }
-
 }
 
 /* md */
@@ -234,16 +228,13 @@ beforeUnmount() {
     height: 2px;
     background-color: black;
   }
-
   .sizelogo {
     width: 110px;
     height: 55px;
   }
-
   .searchsize {
     max-width: 428px;
   }
-
 }
 
 /* lg */
@@ -253,22 +244,18 @@ beforeUnmount() {
     height: 3px;
     background-color: black;
   }
-
   .sizelogo {
     width: 120px;
     height: 60px;
   }
-
   .searchsize {
     max-width: 448px;
   }
 }
 
-
 .dark .menu {
   background-color: #cac3c3;
 }
-
 
 .dark .icon {
   color: #cac3c3;
